4947febd5572562232e1f65b4fc5bf4cedac7c76
kalinchukkristina <89469456+kalinchukkristina@users.noreply.github.com>
2023-05-22 11:48:59 +0200

    Update test_get_receipe.py

--- backend/test/unit/test_get_receipe.py
+++ backend/test/unit/test_get_receipe.py
@@ -1,6 +1,64 @@
-import pytest
+from unittest.mock import MagicMock
+from src.controllers.receipecontroller import ReceipeController
+from src.static.diets import Diet
 
-# add your test case implementation here
 @pytest.mark.unit
-def test():
-    assert False
+@patch("src.util.calculator")
+def test_get_receipe_readiness_with_invalid_diet(calculate_readiness_mock):
+    # test cases to get the readincess for a recipe that has wrong diet
+    calculate_readiness_mock.return_value = 0
+    controller = ReceipeController(items_dao=None)
+
+    receipe = {
+        "name": "cake",
+        "diets": ["vegetarian"],
+    }
+    available_items = {
+        "flour": 100,
+        "egg": 50,
+    }
+    diet = Diet.VEGAN
+    readiness = controller.get_receipe_readiness(receipe, available_items, diet)
+
+    assert readiness is None
+
+
+@pytest.mark.unit
+@patch("src.util.calculator")
+def test_get_receipe_readiness_with_valid_receipe_ingredients_available(calculate_readiness_mock):
+    # test cases to get readiness for a valid recipe with necessary ingredients in the pantry
+    calculate_readiness_mock.return_value = 0.5
+    controller = ReceipeController(items_dao=None)
+
+    receipe = {
+        "name": "cake",
+        "diets": ["vegetarian"],
+    }
+    available_items = {
+        "flour": 100,
+        "egg": 50,
+    }
+
+    diet = Diet.VEGETARIAN
+    readiness = controller.get_receipe_readiness(receipe, available_items, diet)
+    assert readiness == 0.5
+
+@pytest.mark.unit
+@patch("src.util.calculator")
+def test_get_receipe_readiness_with_valid_receipe_ingredients_NOT_available(calculate_readiness_mock):
+    # test cases to get readiness for a valid recipe with ingredients taht are not in the pantry
+    calculate_readiness_mock.return_value = 0.05
+    controller = ReceipeController(items_dao=None)
+
+    receipe = {
+        "name": "cake",
+        "diets": ["vegetarian"],
+    }
+    available_items = {
+        "flour": 100,
+        "egg": 50,
+    }
+
+    diet = Diet.VEGETARIAN
+    readiness = controller.get_receipe_readiness(receipe, available_items, diet)
+    assert readiness is None
